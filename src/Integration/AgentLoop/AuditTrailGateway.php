108017165d20af0a044b11eab72b217ea6538fa3
Lars Moelleken <lars@moelleken.org>
2026-08-25 05:57:32 +0200

    style: satisfy audit timeline formatter

--- src/Integration/AgentLoop/AuditTrailGateway.php
+++ src/Integration/AgentLoop/AuditTrailGateway.php
@@ -90,7 +90,7 @@ final readonly class AuditTrailGateway
         }
         usort(
             $timeline,
-            static fn (AuditTimelineEntry $left, AuditTimelineEntry $right): int => strcmp($right->at, $left->at),
+            static fn(AuditTimelineEntry $left, AuditTimelineEntry $right): int => strcmp($right->at, $left->at),
         );
 
         return new TaskAuditSnapshot(
